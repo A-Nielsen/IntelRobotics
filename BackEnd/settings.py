e6a1c5a3579ff8070c4d2aecfe03a4f4d0e4139a
Titaniummik <ib.mikkel@gmail.com>
2022-11-17 09:44:52 +0100

    Update settings.py
    Added secure keys to settings

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -1,11 +1,16 @@
 import os
+import secrets
+import datetime
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 config['UPLOAD_FOLDER'] = os.path.dirname(os.path.realpath(__file__)) + r'\images'
 
-JWT_SECRET_KEY = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
+JWT_SECRET_KEY = secrets.token_hex(16)
+SECRET_KEY = secrets.token_hex(16)
+JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=60)
+JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=60)
 CORS_HEADERS = 'Content-Type'
 MONGODB_SETTINGS = {
     'host': f"mongodb+srv://{config['user']}:{config['pwd']}@cluster0.geaqzsr.mongodb.net/?retryWrites=true&w=majority"
